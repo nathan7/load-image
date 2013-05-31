41d8c0c5ab4de97f0c2cff030ae3c6e48c969141
Nathan Zadoks <nathan@nathan7.eu>
2013-05-31 17:54:12 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var Promise = require('promise')
+  , isPromise = require('is-promise')
+  , lazify = require('lazify')
+  , Image = this.Image || require('canvas').Image
+
+module.exports = exports =
+function loadImage(src) {
+  if (isPromise(src)) return src.then(loadImage)
+  return new Promise(function(resolve, reject) {
+    var img = new Image()
+    img.onload = function() { resolve(img) }
+    img.onerror = reject
+  })
+}
+
+exports.lazily = function loadImageLazily(src) {
+  return lazify(function() { return loadImage(src) })
+}
